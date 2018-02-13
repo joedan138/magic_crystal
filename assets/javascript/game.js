99e019c73310e8a3c1948b515ef63d870d1720c9
joedan138 <joedan138@gmail.com>
2018-02-12 17:49:22 -0700

    nearly complete submission

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,56 @@
-// function to create and append random number
-
-function randomNumGenerate(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    $("#targetNumber").append(randomNumber)
-};
-
-randomNumGenerate(19,120);
+// intro alert with instructions
+$(document).ready(function() {
+    import swal from '../sweetalert/node_modules/sweetalert'
 
 
-// function to create gem values
-function gemValueArray(length, max) {
-    return Array.apply(null, Array(length)).map(function() {
-        return Math.round(Math.random() * max);
+    swal('hello world');
+
+
+    // function to create and append random number
+    function randomNumGenerate(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+        $("#targetNumber").append(randomNumber)
+    };
+
+    randomNumGenerate(19,120);
+
+
+    // function to create gem values
+    function gemValueArray(length, max) {
+        return Array.apply(null, Array(length)).map(function() {
+            return Math.round(Math.random() * max);
+        });
+    }
+
+    var gemValueArray = gemValueArray(4,12);
+
+    // function to apply gem values as data attributes
+    function createGemValues() {
+        $("#blueGem").attr('data-gemValueArray', gemValueArray[0]);
+        $("#greenGem").attr('data-gemValueArray', gemValueArray[1]);
+        $("#pinkGem").attr('data-gemValueArray', gemValueArray[2]);
+        $("#redGem").attr('data-gemValueArray', gemValueArray[3]);
+    }
+
+    createGemValues();
+
+    // retrieve and add gem values to 'your score' area
+    var yourScore = 0
+    $(".gems").click(function() {
+        var gemValue = ($(this).attr('data-gemValueArray'));
+        gemValue = parseInt(gemValue, 10);
+        yourScore += gemValue
+        $("#currentScore").html(yourScore);
     });
-}
 
-var gemValueArray = gemValueArray(4,12);
 
-// function to apply gem values as data attributes
-function createGemValues() {
-    $("#blueGem").attr('data-gemValueArray', gemValueArray[0]);
-    $("#greenGem").attr('data-gemValueArray', gemValueArray[1]);
-	$("#pinkGem").attr('data-gemValueArray', gemValueArray[2]);
-	$("#redGem").attr('data-gemValueArray', gemValueArray[3]);
-}
+    if ($("#currentScore") === $("#targetNumber")) {
+        alert("you have won")
+    } else if ($("#currentScore") > $("#targetNumber")) {
+        alert("you lose you loser")
+    }
 
-createGemValues();
-
-// retrieve and add gem values to 'your score' area
-var counter = 0
-$(".gems").click(function() {
-    var gemValue = ($(this).attr('data-gemValueArray'));
-    gemValue = parseInt(gemValue, 10);
-    counter += gemValue
-    $("#currentScore").html(counter);
-    // if (counter === ) {
-        
-    // } else {
-        
-    // }
-
+// closing of document.ready
 });
